97441b51c43e3489089c4b12212ac2665eff8dd1
SalinasPaulDev7 <salinaspaul.dev@gmail.com>
2025-09-02 11:31:47 -0300

    feat(card): create generic component

---
+++ src/components/card/Card.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+export const Card = ({ children, style, ...props }) => {
+  return (
+    <View style={[styles.container, style]} {...props}>
+      {children}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    backgroundColor: "#ffffff",
+    borderRadius: 12,
+    borderWidth: 1,
+    borderColor: "#e5e7eb",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+});
